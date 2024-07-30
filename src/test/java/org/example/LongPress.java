f8d522feff2d61ff307aa4725f3b626ea2ac01f8
reema <ralnakhebi@hotmail.com>
2024-07-30 20:27:17 +0300

    Appium Test v.2 long press

--- src/test/java/org/example/LongPress.java
+++ src/test/java/org/example/LongPress.java
@@ -3,6 +3,10 @@ package org.example;
 import Utilities.BaseTest;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.Test;
 
 public class LongPress extends BaseTest {
@@ -12,9 +16,11 @@ public class LongPress extends BaseTest {
         driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Expandable Lists']")).click();
         driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
 
-        //long press javascript Code
-//        driver.executeScript("mobile: longClickGesture",
-//                ImmutableMap.of("elementId", ""))
+        //long press Gesture with javascript Code
+        WebElement longPressButton=driver.findElement(By.xpath("//android.widget.TextView[@text='People Names']"));
+        ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId", ((RemoteWebElement)longPressButton).getId(),
+                        "duration",2000));
 
     }
 }
